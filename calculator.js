349afdec8e82c73f14c3686b6ccfbd8714302a12
Eduardo DuQuesne <eduquesne@gmail.com>
2018-03-04 17:02:20 -0600

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -15,50 +15,21 @@ let func = [add, subtract, multiply, divide];
 let numOne = getId("number1");
 let numTwo = getId("number2");
 
-/*
-Create a function that adds two numbers
-passed in as arguments. Return the sum.
-*/
-
 function add(num1, num2) {
     return Number(num1) + Number(num2);
 }
-
-/*
-  Create a function that subtracts two numbers
-  passed in as arguments. Return the difference.
- */
 
  function subtract(num1, num2) {
     return Number(num1) - Number(num2);
  }
 
-/*
-Create a function that multiplies two numbers
-passed in as arguments. Return the product.
-*/
-
 function multiply(num1, num2) {
     return Number(num1) * Number(num2);
 }
 
-/*
-  Create a function that divides two numbers
-  passed in as arguments. Return the quotient.
- */
-
  function divide(num1, num2) {
      return Number(num1) / Number(num2);
  }
-
-/*
-  Create a function that accepts three arguments.
-    1. First number
-    2. Second number
-    3. A function that performs an operation on them
-
-  Return the value of the operation.
- */
 
 function calculate(num1, num2, func) {
     return func(num1, num2);
